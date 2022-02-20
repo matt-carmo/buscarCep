84bf9940b9e7e4cc0b7f9e5124454ad8468c05c4
matt-carmo <matheus_2018@hotmail.com>
2022-02-20 13:44:18 -0300

    desktop-version

--- js/script.js
+++ js/script.js
@@ -5,15 +5,35 @@ const cidade = document.querySelector("#cidade");
 const uf = document.querySelector("#uf");
 const ibge = document.querySelector("#ibge");
 const btn = document.querySelector(".btn");
+const form = document.querySelector(".form")
+const clear = document.querySelector('.clear')
 
-      
-btn.addEventListener('click' , () => {
-      const cepLength = cep.value.length     
-      if (cepLength == 8 && /^[0-9]+$/.test(cep.value)){
-            getCepsForHtml()
-      } else limparCep(); console.log('oi')
-      
+
+clearCep = cep
+clear.addEventListener('click', () => {
+      cep.value = ''
+      limparCep()
 })
+
+cep.addEventListener('keypress', (e) => {
+      if(e.which == 13){
+            getCepsForHtml();
+      }
+})
+btn.addEventListener("click", () => {
+    const cepLength = cep.value.length;
+    if (cepLength == 8 && /^[0-9]+$/.test(cep.value)) {
+        
+      getCepsForHtml();
+    } else
+    swal.fire({
+      icon: "error",
+      title: "400: Bad Request",
+      text: "CEP deve conter 8 dígitos e SOMENTE números",
+      heightAuto: false,
+  });
+  
+});
 /*
 const getPosts = () => fetch(url)
 
@@ -22,35 +42,45 @@ getPosts().then(response => {
 }) */
 
 limparCep = () => {
-      rua.value = ''
-      bairro.value = ''
-      cidade.value = ''
-      uf.value = ''
-      ibge.value =''
-}
+    rua.value = "";
+    bairro.value = "";
+    cidade.value = "";
+    uf.value = "";
+    ibge.value = "";
+};
 
-const getPosts = async() => {          
-      try{             
-            const cepValue = cep.value
-            url = `https://viacep.com.br/ws/${cepValue}/json/`
-            const response  = await  axios.get(url)
-            return response.data            
-      }catch(error){
-            console.log(alert('Conexão Recusada pelo Servidor'), error)
-      }
-}
+const getPosts = async () => {
+    try {
+        const cepValue = cep.value;
+        url = `https://viacep.com.br/ws/${cepValue}/json/`;
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+      swal.fire({
+            icon: "error",
+            title: "503: Serviço Indisponível",
+            text: "Falha na conexão com ViaCEP",
+            heightAuto: false,
+    })}
+};
 
-const getCepsForHtml = () =>  getPosts().then( value => {
-      console.log(value.erro)      
-      if(value.erro == true) {
-            console.log('Cep não existe(Ou não encontrado)')
-      }else{
-            console.log(value.erro)
-            rua.value = value.logradouro      
-            bairro.value = value.bairro
-            cidade.value = value.localidade
-            uf.value = value.uf
-            ibge.value = value.ibge
-      }  
-})
-
+const getCepsForHtml = () =>
+    getPosts().then((value) => {
+        console.log(value.erro);
+        if (value.erro == true) {
+            swal.fire({
+                icon: "question",
+                title: "CEP Inválido",
+                text: "O CEP que você digitou não existe ou não está cadastrado no banco de dados.",
+                heightAuto: false,
+            });
+            limparCep();
+        } else {
+            console.log(value.erro);
+            rua.value = value.logradouro;
+            bairro.value = value.bairro;
+            cidade.value = value.localidade;
+            uf.value = value.uf;
+            ibge.value = value.ibge;
+        }
+    });
